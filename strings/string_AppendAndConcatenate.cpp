5bb02308ff9f56c4c01a65e9b4ebc708b10d1174
Neeraj Kumar Singh <869neerajsingh@gmail.com>
2021-09-11 22:21:12 +0530

    strings problems

---
+++ strings/string_AppendAndConcatenate.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string st1 = "Comp";
+    string st2 = "uter";
+    // st1.append(st2);
+    // st1 = st1 + st2;
+    // cout<<st1<<endl;
+    cout<<st1+st2<<endl;
+    return 0;
+}
