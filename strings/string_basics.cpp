35a29e35c1fb60bf279b8d54139a7192b859a573
Neeraj Kumar Singh <869neerajsingh@gmail.com>
2021-09-12 23:40:29 +0530

    Strings Problems

---
+++ strings/string_basics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str;
+    // string str1 = "HelloWorld";
+    // string str2(5, 'n');
+    getline(cin, str);
+    cout<<str;
+    return 0;
+}
