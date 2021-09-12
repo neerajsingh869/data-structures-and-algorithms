842d05a364dbf9a25d369bc15daef6b8e4f9fbd2
Neeraj Kumar Singh <869neerajsingh@gmail.com>
2021-09-12 23:41:38 +0530

    Strings Problems

---
+++ strings/string_functions.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>        // for string functions
+#include<algorithm>     // for sort function
+using namespace std;
+
+int main(){
+    string s1 = "software", s2 = "engineer";
+    // s1.clear();
+    
+    // if(s2.compare(s1) != 0){
+    //     cout<<"Strings are not equal"<<endl;
+    // }
+    // cout<<s2.compare(s1)<<endl;
+
+    // if(s1.empty()){
+    //     cout<<"s1 is empty."
+    // }
+    // cout<<s1.empty()<<endl;
+
+    // s1.erase(4, 4);
+
+    // cout<<s1.find("ftwa")<<endl;
+
+    // cout<<s1.insert(4, "happy")<<endl;
+
+    // cout<<s1.length()<<endl;
+
+    // sort(s1.begin(), s1.end());
+    // cout<<s1<<endl;
+
+    cout<<s1<<" "<<s2<<endl;
+    return 0;
+}
