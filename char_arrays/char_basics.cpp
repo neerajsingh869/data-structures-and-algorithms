ed180486877d98dcc81ba584c7078384c2fffa7b
Neeraj Kumar Singh <869neerajsingh@gmail.com>
2021-10-05 23:25:42 +0530

    Character arrays

---
+++ char_arrays/char_basics.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Declaration of character array
+    char arr[10] = "apple";
+    int i = 0;
+    while(arr[i] != '\0'){
+        cout<<arr[i]<<endl;
+        i++;
+    }
+    return 0;
+}
