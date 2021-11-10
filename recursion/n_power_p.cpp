172aeb93e0b30a8f8a67e5bce66b45f7a20de153
Neeraj Kumar Singh <869neerajsingh@gmail.com>
2021-11-10 15:28:50 +0530

    Recursion Problems

---
+++ recursion/n_power_p.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int Power(int n, int p){
+    if(p == 0){
+        return 1;
+    }
+    else{
+        int prevPower = Power(n, p - 1);
+        return n * prevPower;
+    }
+}
+int main(){
+    int n, p;
+    cin>>n>>p;
+    cout<<Power(n, p)<<endl;
+    return 0;
+}
