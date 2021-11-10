d959e008c5ffde06455e51062c76dcbf74ea320d
Neeraj Kumar Singh <869neerajsingh@gmail.com>
2021-11-10 15:26:39 +0530

    Recursion Problems

---
+++ recursion/decreasing_and_increasing.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void dec(int n){
+    if(n==0){
+        return;
+    }
+    cout<<n<<" ";
+    dec(n-1);
+}
+
+void inc(int n){
+    if(n==1){
+        cout<<n<<" ";
+        return;
+    }
+    inc(n-1);
+    cout<<n<<" ";
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    inc(n);
+    return 0;
+}
